56ea16c3bb06c2d930e6d813511bf71565a76a9a
Luciano <serafinoluciano@gmail.com>
2023-02-28 04:21:28 -0300

    primer commit

---
+++ javascript/javascript.js
@@ -0,0 +1,7 @@
+
+const typed = new Typed('.textoEscrito', {
+    strings: ["Programador", "Diseñador", "Papá de Lupe y Luz", "Emprendedor", "Luciano Serafino"],
+    typeSpeed: 30, startDelay: 500, loop: true, loopCount: 2,
+  });
+
+  
